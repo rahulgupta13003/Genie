b6704c132f1069e42de6bdb3310a134b61b8d482
Rahul Gupta <rahulgupta13003@gmail.com>
2025-09-09 16:17:05 +0530

    12:Fragment_View

--- src/modules/projects/ui/components/message-form.tsx
+++ src/modules/projects/ui/components/message-form.tsx
@@ -81,7 +81,7 @@ export const MessageForm = ({ projectId }: Props) => {
                             className="pt-4 resize-none border-none w-full outline-none bg-transparent"
                             placeholder="What would you like to build?"
                             onKeyDown={(e)=>{
-                                if(e.key ==="Enter" && (e.ctrlKey || e.metaKey)) {
+                                if(e.key ==="Enter") {
                                     e.preventDefault();
                                     form.handleSubmit(onSubmit)(e);
                                 }
